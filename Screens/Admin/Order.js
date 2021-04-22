6df841ef298bf05d8d68e25dfec9933e687ab9d3
bhowe111 <brendan.howe1@gmail.com>
2021-04-22 15:26:11 -0700

    commented auth, searching  for api call error

---
+++ Screens/Admin/Order.js
@@ -0,0 +1,44 @@
+import React, { useState, useCallback } from "react";
+import { View, FlatList, Text } from "react-native";
+import axios from "axios";
+import baseURL from "../../assets/common/baseUrl";
+import { useFocusEffect } from "@react-navigation/native";
+
+import OrderCard from "../../Shared/OrderCard";
+
+const Orders = (props) => {
+  const [orderList, setOrderList] = useState();
+
+  useFocusEffect(
+    useCallback(() => {
+      getOrders();
+      return () => {
+        setOrderList();
+      };
+    }, [])
+  );
+
+  const getOrders = () => {
+    axios
+      .get(`${baseURL}orders`)
+      .then((x) => {
+        setOrderList(x.data);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <View>
+      <FlatList
+        data={orderList}
+        renderItem={({ item, index }) => (
+          <OrderCard navigation={props.navigation} {...item} editMode={true} />
+        )}
+        // was previously keyExtractor={(item) => item.id}
+        keyExtractor={(item, index) => item.id}
+      />
+    </View>
+  );
+};
+
+export default Orders;
---
+++ Shared/Banner.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import { Image, StyleSheet, Dimensions, View, ScrollView } from "react-native";
+import Swiper from "react-native-swiper/src";
+
+var { width } = Dimensions.get("window");
+
+const Banner = () => {
+  const [bannerData, setBannerData] = useState([]);
+
+  useEffect(() => {
+    setBannerData([
+      "https://images.pexels.com/photos/2106216/pexels-photo-2106216.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      "https://www.sportrx.com/sportrx-blog/wp-content/uploads/2019/07/real-ray-bans-vs-fake-ray-bans.jpg",
+      "https://shershegoes.com/wp-content/uploads/best-chelsea-boots-womens.jpg",
+      "https://empire-s3-production.bobvila.com/articles/wp-content/uploads/2021/01/best_garden_trowel-main.jpg",
+    ]);
+
+    return () => {
+      setBannerData([]);
+    };
+  }, []);
+
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.swiper}>
+          <Swiper
+            style={{ height: width / 2 }}
+            showButtons={false}
+            autoplay={true}
+            autoplayTimeout={4}
+          >
+            {bannerData.map((item) => {
+              return (
+                <Image
+                  key={item}
+                  style={styles.imageBanner}
+                  resizeMode="contain"
+                  source={{ uri: item }}
+                />
+              );
+            })}
+          </Swiper>
+          <View style={{ height: 20 }}></View>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "gainsboro",
+  },
+  swiper: {
+    width: width,
+    alignItems: "center",
+    marginTop: 10,
+  },
+  imageBanner: {
+    height: width / 2,
+    width: width - 40,
+    borderRadius: 10,
+    marginHorizontal: 20,
+  },
+});
+
+export default Banner;
